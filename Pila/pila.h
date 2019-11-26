2fedd86922d149260b628d7aaf7fdac6205b2ab7
Miguel Montoya <43622975+Miguel056@users.noreply.github.com>
2019-11-26 10:42:07 -0600

    Update pila.h

--- Pila/pila.h
+++ Pila/pila.h
@@ -2,10 +2,10 @@
 #define __PILA_H__
 #include<stdio.h>
 #include<stdlib.h>
-struct pila
-{
-  int dato;
-  struct pila *siguiente;
+struct pila{
+	int dato;
+	struct pila *siguiente;
+	struct pila *anterior;
 };
 void push (struct pila **, int);
 void mostrar (struct pila *);
